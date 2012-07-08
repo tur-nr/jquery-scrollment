ed68c3fdb2f972a8f64fb5c525572c716f76cbbe
Christopher Turner <turner296@gmail.com>
2012-07-08 15:21:04 +0100

    source, readme and license

--- readme.md
+++ readme.md
@@ -1,20 +1,156 @@
 # Scrollment
 * * *
+
 ## Read Me
+
 ### What is Scrollment
+
 Scrollment is a jQuery plugin that allows CSS manipulation as you scroll. Just set the start and stop values of the scroll bar and the CSS properties and watch the elements transform as you scroll.
+
+* * *
+
 ### Requirements
-jQuery v1.4
-*IE support unknown*
+
+* jQuery v1.4.4
+
+* * *
+
 ### Usage
 
+To use Scrollment just include `jquery.scrollment-1.0.js` into your webpage (and jQuery first of course), and use it like any other plugin.
+
+#### Example
+
     $('#element').scrollment({
-        start: 0,
-        stop: 200,
+        start: 50,
+        stop: 150,
         css: {
             left: {
                 start: 0,
                 stop: 200
             }
         }
-    })
+    })​
+
+The above example will move `#element` left from `0px` to `200px` when you scroll between `50px` and `150px`.
+
+Try: [http://jsfiddle.net/CLCYx/3/](http://jsfiddle.net/CLCYx/3/)
+
+* * *
+
+### Options
+
+#### Basic
+
+* `start` This is the starting value of the scroll.
+* `stop` The stopping value of the scroll.
+* `css` Object hash containing a collection of *available* CSS properties.
+* `id` *Optional* Scrollment identifier, used for managing []multiple](#multiple-options) options and [methods](#methods).
+* `forceStart` *Optional* Forces the element to transform to it's starting properties.
+
+##### CSS Properties
+
+The CSS properties available are basically any that handle numeric values (`width`, `height`, `top`, `left`, etc). It will treat all values as pixels and you can set fraction values like opacity using `1` and `0` also. Each property **must** have a `start` and `stop` value.
+
+**Note:** Use only exact pixels, see [Closure Values](#closure-values) for alternative ways to setting values.
+
+#### Animation
+
+You can also add animation to your Scrollment, just add the `animation` to your options and set the below.
+
+* `duration` The duration of the animation.
+* `easing` *Optional* Adds easing to the animation, defaults to `swing`. See http://gsgd.co.uk/sandbox/jquery/easing/.
+
+Example: [http://jsfiddle.net/CLCYx/4/](http://jsfiddle.net/CLCYx/4/)
+
+#### Callbacks (Hooks)
+
+Scrollment can also inform your webpage when scrolling starts, finishes and whilst scrolling is taking place.
+
+* `onStart` When scrolling begins.
+* `onStop` When the scrolling is finished.
+* `onScroll` Whilst scrolling is taking place.
+
+All callbacks have the following function definition and must be added to the options.
+
+    function (current, last) {
+        // current: the current scroll value
+        // last: the last scroll value 
+    }
+
+Each callback is invoked after the CSS has been set, but be aware that animations wont have finished. You can determine whether the scroll is near the start or end of its Scrollment using the `current` parameter. The `current` value will be `0` at the start, or equal to `last` at the end.
+
+Example: [http://jsfiddle.net/CLCYx/6/](http://jsfiddle.net/CLCYx/6/)
+
+#### Closure Values
+
+Depending on how complex your webpage is you may want values to differ from time to time, but you still want the existing Scrollment to take place. Replacing values as closures gives you great versatility to alter the values but keep the same Scrollment settings.
+
+    $('#element').scrollment({
+        start: 0,
+        stop: function() {
+            return ($('body').height() - $(window).height())
+        },
+        css: {
+            height: {
+                start: 75,
+                stop: function() {
+                    return ($(window).height() * 0.4)
+                }
+            },
+            width: {
+                start: 75,
+                stop: function() {
+                    return ($(window).height() * 0.8)
+                }
+            }
+        }
+    })​
+
+Example: [http://jsfiddle.net/CLCYx/7/](http://jsfiddle.net/CLCYx/7/)
+
+#### Multiple Options
+
+You can attach as many as you like to a single element by either chaining the plugin method or passing an *Array* of options. When creating multiple Scrollments make use of the `id` option so that you reference singular instances using [methods](#methods).
+
+* * *
+
+### Methods
+
+Scrollment also has a few methods available for enabling, disabling and removing elements.
+
+#### Enabling
+
+    // Disables all attached Scrollments
+    $('#element').scrollment('disable')
+    
+    // Re-enables Scrollment with the id: demo
+    $('#element').scrollment('enable', 'demo')
+
+#### Removing
+
+    // Removes all
+    $('#element').scrollment('remove')
+    
+    // Removes only demo_one, demo_two
+    $('#element').scrollment('remove', ['demo_one', 'demo_two'])
+
+#### Scrollment Object
+
+The Scrollment is a single object and set globally on the jQuery object, so you read over the code and hack away.
+
+    $.Scrollment
+
+* * *
+
+### Feedback
+
+If you are using the plugin for anything, have ideas for improving it or simply want to talk out about how and why it works give me a tweet [@tur_nr](http://twitter.com/tur_nr).
+
+This is my first jQuery plugin so I appreciate any feedback whatsoever, issues, praises or criticism I'll take it all onboard.
+
+* * *
+
+### Copyright
+
+Copyright Christopher Turner, 2012 Licensed under the [MIT license](http://www.opensource.org/licenses/mit-license.php).
--- src/jquery.scrollment-1.0.js
+++ src/jquery.scrollment-1.0.js
@@ -1,69 +1,77 @@
+// Scrollment
+// jQuery Plugin
+//
+// @version    1.0
+// @author     Christopher Turner
+// @copyright  Copyright (c) 2012 Christopher Turner
+// @link       https://github.com/tur-nr/jquery-scrollment
+
 (function ($, window, undefined) {
 	$.Scrollment = new function () {
-		this.$win        = $(window)
-		this.initiated   = false
-		this.unique      = 0
-		this.attachments = {}
-		this.stack       = []
+		this.$win        = $(window);
+		this.initiated   = false;
+		this.unique      = 0;
+		this.attachments = {};
+		this.stack       = [];
 
 		this.init = function () {
 			// check if already initiated
 			if (! this.initiated) {
-				this.$win.bind('scroll.scrollment', $.proxy(this.onScroll, this))
+				this.$win.bind('scroll.scrollment', $.proxy(this.onScroll, this));
 
-				setTimeout($.proxy(this.onScroll, this), 0)
+				setTimeout($.proxy(this.onScroll, this), 0);
 			}
 
-			return this.initiated = true
+			return this.initiated = true;
 		}
 
 		this.bindElement = function ($element, settings) {
 			// scrollment reference
-			var that = this
+			var that = this;
 
 			$element.each(function () {
 				var attachment = {
 						 '$element': $(this),
 						'settings': $.extend({
-							            forceStart: false,
+							            forceStart: false
 							        }, settings),
 						     'get': function (key) {
 						     	    	// helpers to return a defined value
 										// or to call a function and get its returned value
 										var value = function (k) {
-											return this.settings[k]
-										}
-										, func = function (k) {
-											return this.settings[k].apply(this.$element)
-										}
+												return this.settings[k];
+											}
+											, func = function (k) {
+												return this.settings[k].apply(this.$element);
+											};
 
 										if (typeof this.settings[key] === 'function') {
-											return func.apply(this, [key])
+											return func.apply(this, [key]);
 										}
 										else {
-											return value.apply(this, [key])
+											return value.apply(this, [key]);
 										}
 									},
 							 'css': function (key, state) {
 						     	    	// helpers to return a defined value
 										// or to call a function and get its returned value
 										var value = function (k, s) {
-											return this.settings.css[k][s]
-										}
-										, func = function (k, s) {
-											return this.settings.css[k][s].apply(this.$element)
-										}
+												return this.settings.css[k][s];
+											}
+											, func = function (k, s) {
+												return this.settings.css[k][s].apply(this.$element);
+											};
 
 										if (typeof this.settings.css[key][state] === 'function') {
-											return func.apply(this, [key, state])
+											return func.apply(this, [key, state]);
 										}
 										else {
-											return value.apply(this, [key, state])
+											return value.apply(this, [key, state]);
 										}
 									},
 						 'trigger': function (key, params) {
 										if (typeof this.settings[key] === 'function') {
-											this.settings[key].apply(this.$element, Array.prototype.slice.call(arguments, 1))
+											this.settings[key].apply(this.$element, Array.prototype.slice.call(arguments, 1));
 										}
 									},
 						    'data': {
@@ -72,89 +80,89 @@
 						    }
 				    }
 				  , id  = attachment.get('id') || 'scrollment-'+(++that.unique)
-				  , ids = attachment.$element.data('scrollment-ids')
+				  , ids = attachment.$element.data('scrollment-ids');
 
-				if (ids === undefined) ids = [id]
-				else ids.push(id)
+				if (ids === undefined) ids = [id];
+				else ids.push(id);
 
-				attachment.$element.data('scrollment-ids', ids)
+				attachment.$element.data('scrollment-ids', ids);
 
 				// force start
 				if (attachment.get('forceStart') === true) {
-					var css = {}
+					var css = {};
 
 					$.each(attachment.get('css'), function (property, options) {
-						css[property] = attachment.css(property, 'start')
-					})
+						css[property] = attachment.css(property, 'start');
+					});
 
-					attachment.$element.css(css)
+					attachment.$element.css(css);
 				}
 
 				// add to scrollment attachment
-				that.attachments[id] = attachment
-			})
+				that.attachments[id] = attachment;
+			});
 		}
 
 		this.unbindElement = function ($element, id) {
-			var ids = $element.data('scrollment-ids')
+			var ids = $element.data('scrollment-ids');
 
 			// no scrollment in place
 			if (ids === undefined)
-				return $element
+				return $element;
 			else {
 				if (id === undefined)
-					id = ids
+					id = ids;
 				else if (Object.prototype.toString.call(id) !== '[object Array]')
-					id = [id]
+					id = [id];
 			}
 
 			// remove attachment and data
 			for (var i = 0; i < id.length; i++) {
-				delete this.attachments[id[i]]
-				ids.remove(id[i])
+				delete this.attachments[id[i]];
+				ids.remove(id[i]);
 			}
 
-			this.refreshStack()
+			this.refreshStack();
 		}
 
 		this.enableElement = function ($element, id, flag) {
-			var ids  = $element.data('scrollment-ids')
+			var ids  = $element.data('scrollment-ids');
 
 			// no scrollment in place
 			if (ids === undefined)
-				return $element
+				return $element;
 			else {
 				if (id === undefined)
-					id = ids
+					id = ids;
 				else if (Object.prototype.toString.call(id) !== '[object Array]')
-					id = [id]
+					id = [id];
 			}
 
 			// enabled the scrollment
 			for (var i = 0; i < id.length; i++) {
-				this.attachments[id[i]].data.enabled = flag
+				this.attachments[id[i]].data.enabled = flag;
 			}
 		}
 
 		this.refreshStack = function (scrollTop) {
-			var that = this // scrollment ref
+			var that = this; // scrollment ref
 
-			scrollTop  = scrollTop || this.$win.scrollTop()
-			this.stack = []
+			scrollTop  = scrollTop || this.$win.scrollTop();
+			this.stack = [];
 
 			$.each(this.attachments, function (key, attachment) {
 				var start = attachment.get('start')
-				  , stop  = attachment.get('stop')
+				  , stop  = attachment.get('stop');
 
 				if ((scrollTop >= start && scrollTop <= stop) || attachment.data.scrolling) {
 					// add to stack
-					that.stack.push(key)
+					that.stack.push(key);
 				}
-			})
+			});
 		}
 
 		this.onScroll = function (event) {
-			var scrollTop = this.$win.scrollTop()
+			var scrollTop = this.$win.scrollTop();
 
 			for (var i = 0; i < this.stack.length; i++) {
 				var attachment = this.attachments[this.stack[i]]
@@ -168,13 +176,13 @@
 				  				       'jump': false
 								 }, attachment.get('animation'))
 				  , animate    = (typeof animation.duration === 'number')
-				  , css        = {}
+				  , css        = {};
 
 				if (attachment.data.enabled === false) continue;
 
 				// precise frame
-				if (frame < 0) frame = 0
-				else if (frame > frames) frame = frames
+				if (frame < 0) frame = 0;
+				else if (frame > frames) frame = frames;
 
 
 				$.each(attachment.get('css'), function (property, options) {
@@ -182,86 +190,86 @@
 					  , stop  = attachment.css(property, 'stop')
 					  , diff  = Math.abs(start-stop)
 					  , add   = (diff/frames)*frame
-					  , value
+					  , value;
 
 					// subtract
 					if (start > stop) {
-						add = -add
+						add = -add;
 					}
 
-					css[property] = start+add
+					css[property] = start+add;
 				})
 
 				// animate or not ??
 				if (animate && attachment.data.first !== true)
 					attachment.$element.stop(animation.clear, animation.jump)
-				                       .animate(css, animation.duration, animation.easing)
+				                       .animate(css, animation.duration, animation.easing);
 				else {
-					attachment.$element.css(css)
-					attachment.data.first = false
+					attachment.$element.css(css);
+					attachment.data.first = false;
 				}
 
 				// callbacks
 				// started
 				if (! attachment.data.scrolling) {
-					attachment.trigger('onStart')
-					attachment.data.scrolling = true
+					attachment.trigger('onStart', frame, frames);
+					attachment.data.scrolling = true;
 
 					// on frame
-					attachment.trigger('onScroll', frame, frames)
+					attachment.trigger('onScroll', frame, frames);
 				}
 				else {
 					// on frame
-					attachment.trigger('onScroll', frame, frames)
+					attachment.trigger('onScroll', frame, frames);
 
 					// stopped
 					if (frame == 0 || frame == frames) {
-						attachment.trigger('onStop')
-						attachment.data.scrolling = false
+						attachment.trigger('onStop', frame, frames);
+						attachment.data.scrolling = false;
 					}
 				}
 			}
 
 			// refresh stack
-			this.refreshStack(scrollTop)
+			this.refreshStack(scrollTop);
 		}
-	}
+	};
 
 	$.fn.scrollment = function (options) {
 		var $element = this
-		  , id       = arguments[1]
+		  , id       = arguments[1];
 
 		if (typeof options === 'string') {
 			// check if scrollment is on element
 			switch (options) {
-				case 'start':
-					$.Scrollment.enableElement($element, id, true)
-					break
+				case 'enable':
+					$.Scrollment.enableElement($element, id, true);
+					break;
 
-				case 'stop':
-					$.Scrollment.enableElement($element, id, false)
-					break
+				case 'disable':
+					$.Scrollment.enableElement($element, id, false);
+					break;
 
 				case 'remove':
-					$.Scrollment.unbindElement($element, id)
-					break
+					$.Scrollment.unbindElement($element, id);
+					break;
 			}
 
-			return $element
+			return $element;
 		}
 		// force options to be type array
-		if (Object.prototype.toString.call(options) !== '[object Array]') options = [options]
+		if (Object.prototype.toString.call(options) !== '[object Array]') options = [options];
 
 		// attach element(s) to scrollment
 		$.each(options, function (i, settings) {
-			$.Scrollment.bindElement($element, settings)
-		})
+			$.Scrollment.bindElement($element, settings);
+		});
 
 		// enable scrollment plugin
-		$.Scrollment.init()
+		$.Scrollment.init();
 
-		return $element
-	}
+		return $element;
+	};
 
 	// we need this to remove array elements
 	Array.prototype.remove = function() {
@@ -273,7 +281,7 @@
 	        }
 	    }
 	    return this;
-	}
+	};
 	// IE8 indexOf
 	Array.prototype.indexOf = function(what, i) {
         i= i || 0;
@@ -283,5 +291,5 @@
             ++i;
         }
         return -1;
-    }
-})(jQuery, window)
+    };
+})(jQuery, window);
